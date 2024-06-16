ef76868ce9ad91c5f0ab80efcdd337d8356fbd57
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-16 03:01:43 +0000

    Example 4 - Converting seconds to minutes and seconds

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-// Chapter 2 Examples
-#include <iostream>
+// Example 4 - Converting Seconds to Minutes and Seconds
+//
 
-int main() {
-  std::cout << "Chapter 2 Examples\n";
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int totalSeconds, minutes, seconds;
+
+    cout << "Enter a number of seconds between 0 and 3600: ";
+    cin >> totalSeconds;
+
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+
+    cout << minutes << " minutes and " << seconds << " seconds" << endl;
+
+    return 0;
 }
+
