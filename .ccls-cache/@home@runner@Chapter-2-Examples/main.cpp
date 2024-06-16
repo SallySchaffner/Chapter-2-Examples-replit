641d87cf077803575a511cc3ce527506bb58f097
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-16 02:59:24 +0000

    Example 3 - Extracting Digits

--- .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
@@ -1,6 +1,26 @@
-// Chapter 2 Examples
-#include <iostream>
+// Example 3 - Extracting Digits 
+//
 
-int main() {
-  std::cout << "Chapter 2 Examples\n";
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number, digit1, digit2, digit3, digit4;
+
+  cout << "Enter a 4-digit number: ";
+  cin >> number;
+  digit1 = number % 10;  //Extract the first digit
+  number = number / 10;  //Discard the rightmost digit
+  digit2 = number % 10;  //Extract the new rightmost digit
+  number = number / 10;  //Discard the rightmost digit
+  digit3 = number % 10;  //Extract the new rightmost digit
+  number = number / 10;  //Discard the rightmost digit
+  digit4 = number;       //Last remaining digit
+
+  // Output in reverse order
+  cout << digit4 << " " << digit3 << " " << digit2 << " " << digit1 << endl;
+
+  return 0;
 }
+
