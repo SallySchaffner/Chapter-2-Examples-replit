9d3015e42f8e0f45088faf392ee6d613170417b0
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-16 02:56:59 +0000

    Example 2 - Percentage Discount

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-// Chapter 2 Examples
-#include <iostream>
+// Example 2 - Percentage Discount
+//
 
-int main() {
-  std::cout << "Chapter 2 Examples\n";
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double regularPrice, percentDiscount, discountAmount, reducedPrice;
+
+  cout << "Enter the regular price of the item: ";
+  cin >> regularPrice;
+  cout << "Enter the discount percentage: ";
+  cin >> percentDiscount;
+
+  discountAmount = regularPrice * percentDiscount / 100;
+  reducedPrice = regularPrice - discountAmount;
+
+  cout << "The discount amount is $" << discountAmount << endl;
+  cout << "The discounted price is $" << reducedPrice;
+
+  return 0;
 }
