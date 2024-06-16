884e51ca4de7a17577599ec7aad0d4bcf069a492
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-16 02:53:55 +0000

    Example 1 - Calculating Percentages

--- .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
@@ -1,6 +1,23 @@
-// Chapter 2 Examples
-#include <iostream>
+// Calculating Percentages
+//
 
-int main() {
-  std::cout << "Chapter 2 Examples\n";
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double salary = 6000;
+    double percent1 = 5, percent2 = 7, percent3 = 10;
+    double contribution1, contribution2, contribution3;
+
+    contribution1 = salary * percent1 / 100;
+    contribution2 = salary * percent2 / 100;
+    contribution3 = salary * percent3 / 100;
+
+    cout << "Amount of contribution at " << percent1 << "% is $" << contribution1 << endl;
+    cout << "Amount of contribution at " << percent2 << "% is $" << contribution2 << endl;
+    cout << "Amount of contribution at " << percent3 << "% is $" << contribution3 << endl;
+
+    return 0;
 }
+
